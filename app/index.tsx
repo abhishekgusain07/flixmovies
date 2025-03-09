a2f84307310589a90589e54f94e1dc8020e57317
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-03-09 13:22:50 +0530

    custom themes

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ export default function Index() {
     <View
       className="flex-1 justify-center items-center"
     >
-      <Text className="text-5xl text-blue-500">welcome</Text>
+      <Text className="text-5xl text-primary">welcome!</Text>
     </View>
   );
 }
